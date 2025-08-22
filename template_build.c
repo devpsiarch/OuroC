98d792eeb5b1d04c49d19d6f36a5733183694307
devpsiarch <hmayoubaty@gmail.com>
2025-08-22 11:35:14 +0100

    added: template build file

---
+++ template_build.c
@@ -0,0 +1,20 @@
+#include "ouroc.h"
+#define OUROC_IMPLI
+
+#define FILE_TO_BUILD "main.cpp"
+#define SOLUTION_BIN "main"
+
+int main(void){
+    OUROC(main,SOLUTION_BIN,FILE_TO_BUILD);
+    OUROC_BUILD_CMD(&main,"g++ -Wall -Wextra",FILE_TO_BUILD,"-o",SOLUTION_BIN);
+
+    OUROC(run,NULL,SOLUTION_BIN);
+    OUROC_BUILD_CMD(&run,"./"SOLUTION_BIN);
+
+    ouroc_run_cmd(&main);
+    ouroc_run_cmd(&run);
+
+    OUROC_KILL(&main);
+    OUROC_KILL(&run);
+    return 0;
+}
